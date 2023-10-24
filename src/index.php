37c5a356413fcfb27c6aa9a5ee19dc0453635413
carlospozo1995 <carlospozo95@gmail.com>
2023-10-23 20:40:30 -0500

    probando compra payphone

--- src/index.php
+++ src/index.php
@@ -6,6 +6,37 @@
 	<title>page store</title>
 </head>
 <body>
-	<h1>RENDER.COM</h1>
+	<button id="buy_btn">click me</button>
+	<script src="jquery.min.js"></script>
+	<script>
+		$(document).ready(function () {
+			$('#buy_btn').click(function () {
+
+				var parametros ={
+					amount: 18.20 * 100,
+					amountWithoutTax: 18.20*100,
+					clientTransactionID: "24sd453f4sd6f118sd6fsd34sd",
+					responseUrl: "https://page-store.onrender.com/payphone.php",
+					cancellationUrl: "https://page-store.onrender.com/payphone.php"
+				};
+
+				$.ajax({
+				    data: parametros,
+				    url: 'https://pay.payphonetodoesposible.com/api/button/Prepare',
+				    type:'POST',
+				    beforeSend:function(xhr){
+					    xhr.setRequestHeader ('Authorization', "Bearer A9IblYZbxZFqSIlahmjnYea1CeAKEFWZoUrNtux1YOeQV-fhvTQ7ouwKqYhYK1vQIqJy165MCSGuANYVJDzdzFW1f2_5M24mlmA4iedc0Ii5h4fQkXilX-4PTxlNq7VzJdAblwueJs2RVWieA6-e8AZnB-zSu5G2dEUkVhVxt9gKdUOLYT4MWK1TVUFPayfwumHLOwhqMeuzkE9CJmSlyQjbUTXc_-ofXi4G8qV1Zfyg4ABO7e03p_e3FiK-v3bdIhQZBUom6dOXOSA7LKMgL_3I-vHkCEB-U1DQhhb4C9a0gGrMeZxysUNdbYpuqHtxQ8-ApQ")
+					},
+				    success:function SolicitarPago(respuesta){
+				      location.href = respuesta.payWithCard;
+				    }, 
+				    error: function(mensajeerror){
+				     	alert ("Error en la llamada:" + mensajeerror.Message);
+				    }
+			  	});
+
+			})
+		})
+	</script>
 </body>
 </html>
--- src/payphone.php
+++ src/payphone.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>payphone</h1>
-</body>
-</html>
+<?php
+//Obtener parametros de la URL enviados por PayPhone
+$transaccion = $_GET["id"];
+$client = $_GET["clientTransactionId"];
+
+//Preparar JSON de llamada
+$data_array = array(
+"id"=> (int)$transaccion,
+"clientTxId"=>$client );
+
+$data = json_encode($data_array);
+
+//Iniciar Llamada
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://pay.payphonetodoesposible.com/api/button/V2/Confirm");
+curl_setopt($curl, CURLOPT_POST, 1);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+curl_setopt_array($curl, array(
+CURLOPT_HTTPHEADER => array(
+"Authorization: Bearer A9IblYZbxZFqSIlahmjnYea1CeAKEFWZoUrNtux1YOeQV-fhvTQ7ouwKqYhYK1vQIqJy165MCSGuANYVJDzdzFW1f2_5M24mlmA4iedc0Ii5h4fQkXilX-4PTxlNq7VzJdAblwueJs2RVWieA6-e8AZnB-zSu5G2dEUkVhVxt9gKdUOLYT4MWK1TVUFPayfwumHLOwhqMeuzkE9CJmSlyQjbUTXc_-ofXi4G8qV1Zfyg4ABO7e03p_e3FiK-v3bdIhQZBUom6dOXOSA7LKMgL_3I-vHkCEB-U1DQhhb4C9a0gGrMeZxysUNdbYpuqHtxQ8-ApQ", "Content-Type:application/json"),
+));
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($curl);
+if (curl_errno($curl)) {
+    echo 'Error en la solicitud cURL: ' . curl_error($curl);
+}
+curl_close($curl);
+
+//En la variable result obtienes todos los parámetros de respuesta
+echo $result;
+?>
